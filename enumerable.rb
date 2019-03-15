a0a737c8fc07829df0bde5e40a8465d92e5183b6
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-03-15 14:14:33 +0100

    My Select

--- enumerable.rb
+++ enumerable.rb
@@ -10,18 +10,33 @@ module Enumerable
     self
   end
 
-    def my_each
-      i = 0
-      if block_given?
-        while i < self.length
-          yield(self[i])
-          i += 1
-        end
+  def my_each
+    i = 0
+    if block_given?
+      while i < self.length
+        yield(self[i])
+        i += 1
       end
-      self
     end
-
+    self
   end
+  
+  def my_select
+    i = 0
+    result = []
+    if block_given?
+      while i < self.length
+        value = yield(self[i])
+        if value
+          result << self[i]
+        end
+        i += 1
+      end
+      return result
+    end
+    self
+  end
+end
   arr = [88, 77, 98, 56]
   arr.my_each { |num| print "#{num}! " }
   arr.my_each_with_index do |num, index|
